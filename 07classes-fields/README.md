806ec4784bd4a4016c545b5a0ff431060a3c7b0c
Mikhail <newlevelcenter@gmail.com>
2020-09-01 20:48:12 +0300

    classes-method

---
+++ 07classes-fields/README.md
@@ -0,0 +1,9 @@
+# Текст задачи
+class ThreeAdder:
+    name = "**Mikhail**" *Пусть участник пишет своё имя, т.е. там должна быть любая строка*
+    age = 23
+    rate = 1234
+
+a = ThreeAdder()
+
+print(a.name)
---
+++ project/project.py
@@ -0,0 +1,16 @@
+class Record:
+    name = "Mikhail"
+    phone = "+380631234567"
+    rate = "2500"
+
+    def get_name(self):
+        return self.phone
+
+    def get_phone(self):
+        return self.phone
+
+    def get_rate(self):
+        return self.rate
+
+b =Record()
+print(b.get_name())
