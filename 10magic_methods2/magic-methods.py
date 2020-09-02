9c467c03f7a1184bb2683f3d38ebdebd4d4d418f
Mikhail <newlevelcenter@gmail.com>
2020-09-02 13:06:22 +0300

    magic_methods2_2

--- 10magic_methods2/magic-methods.py
+++ 10magic_methods2/magic-methods.py
@@ -1,11 +1,11 @@
-class Record:
-    def __init__(self):
-        self.fields = []
-        self.name = "Oleg"
-        self.phone = "+380631234567"
-        self.skill = "Python"
-        self.city = "Kyiv"
-        self.rate = 2000
+class Record:  
+    def __init__(self):  
+        self.fields = []  
+        self.name = "Oleg"  
+        self.phone = "+380631234567"  
+        self.skill = "Python"  
+        self.city = "Kyiv"  
+        self.rate = 2000  
 
     def __eq__(self, other):
         return self.rate == other.rate    
@@ -14,10 +14,10 @@ class Record:
     def __lt__(self, other):
         return self.rate < other.rate
 
-b = Record()
-c = Record()
-c.name = "Igor"
-c.rate = 3000
+b = Record()  
+c = Record()  
+c.name = "Igor"  
+c.rate = 3000  
 
 if c>b:
     print (f"{c.name} зарабатывает больше чем {b.name}")
