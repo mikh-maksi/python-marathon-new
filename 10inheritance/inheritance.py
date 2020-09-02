6b66f8b721cfd090b8c7398ae427f955bcb437b0
Mikhail <newlevelcenter@gmail.com>
2020-09-02 10:27:44 +0300

    inhertance

---
+++ 10inheritance/inheritance.py
@@ -0,0 +1,20 @@
+class DataField:
+    field_description = "General"
+
+    def __init__(self, value):
+        self.value = None
+        self.validate(value)
+
+    def validate(self, value):
+        self.value = value
+
+    def __contains__(self, item):
+        return item in self.value
+
+
+class FirstNameField(DataField):
+    field_description = "Name"
+
+
+class CityField(DataField):
+    field_description = "City"
